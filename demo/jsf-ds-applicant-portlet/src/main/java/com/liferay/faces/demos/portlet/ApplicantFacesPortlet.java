9570228eb6b39dc78f5d6712500f5aeb8dc9cdc3
Neil Griffin <neil.griffin.scm@gmail.com>
2016-06-03 14:28:48 -0400

    FACES-2663 Develop the jsf-ds-applicant-portlet demo (refactor demo as legacy war rather than pre-configured wab)

--- demo/jsf-ds-applicant-portlet/src/main/java/com/liferay/faces/demos/portlet/ApplicantFacesPortlet.java
+++ demo/jsf-ds-applicant-portlet/src/main/java/com/liferay/faces/demos/portlet/ApplicantFacesPortlet.java
@@ -70,8 +70,9 @@ public class ApplicantFacesPortlet extends GenericFacesPortlet {
 		super.init(portletConfig);
 	}
 
-	// Workaround for LPS-66225: This method (along with the corresponding servlet init-param in web.xml) is necessary
-	// in order to ensure that the context listeners have been called before the init(PortletConfig) method is called.
+	// Workaround for LPS-66225:
+	// This method (along with the corresponding init-param in web.xml) is necessary in order to ensure that the
+	// context listeners have been called before the init(PortletConfig) method is called.
 	@Reference(target = "(servlet.init.portlet-class=com.liferay.faces.demos.portlet.ApplicantFacesPortlet)")
 	protected void setServlet(Servlet servlet) {
 		logger.debug("context listeners initialized");
