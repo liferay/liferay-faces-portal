4acf889415a72cad2536e612fb38f29ba9b650d2
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-13 19:36:16 -0400

    FACES-2799 FacesContextHelperPortletImpl does not have a corresponding factory

--- portal/src/main/java/com/liferay/faces/portal/context/internal/FacesContextHelperPortletImpl.java
+++ portal/src/main/java/com/liferay/faces/portal/context/internal/FacesContextHelperPortletImpl.java
@@ -15,56 +15,38 @@ package com.liferay.faces.portal.context.internal;
 
 import java.io.Serializable;
 
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import javax.portlet.PortletRequest;
-import javax.portlet.PortletSession;
 
-import com.liferay.faces.util.context.internal.FacesContextHelperImpl;
+import com.liferay.faces.util.context.FacesContextHelper;
+import com.liferay.faces.util.context.FacesContextHelperWrapper;
 
 
 /**
  * @author  Neil Griffin
  */
-public class FacesContextHelperPortletImpl extends FacesContextHelperImpl implements Serializable {
+public class FacesContextHelperPortletImpl extends FacesContextHelperWrapper implements Serializable {
 
 	// serialVersionUID
 	private static final long serialVersionUID = 3890575634255388174L;
 
-	/** JSR-168/286 request attribute that contains an instance of javax.portlet.PortletRequest */
-	private static final String REQUEST_ATTR_PORTLET_REQUEST = "javax.portlet.request";
+	// Private Data Members
+	private FacesContextHelper wrappedFacesContextHelper;
 
-	@Override
-	public Object getRequestAttribute(String name) {
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-		PortletRequest portletRequest = (PortletRequest) externalContext.getRequest();
-
-		return portletRequest.getAttribute(name);
-	}
-
-	@Override
-	public String getRequestContextPath() {
-		return FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
-	}
-
-	@Override
-	public String getRequestParameter(String name) {
-		PortletRequest portletRequest = (PortletRequest) FacesContext.getCurrentInstance().getExternalContext()
-			.getRequest();
-
-		return portletRequest.getParameter(name);
+	public FacesContextHelperPortletImpl(FacesContextHelper facesContextHelper) {
+		this.wrappedFacesContextHelper = facesContextHelper;
 	}
 
 	@Override
 	public String getRequestQueryString() {
 
-		// Some portlet bridges (like the ICEfaces bridge) wrap the portal's
-		// PortletRequest implementation instance (which prevents us from getting
-		// the query_string). As a workaround, we can still get it the original
-		// PortletRequest instance, because the Portlet spec says it must be
-		// stored in the javax.portlet.request attribute.
+		// Some portlet bridges (like the ICEfaces bridge) wrap the portal's PortletRequest implementation instance
+		// (which prevents us from getting the query_string). As a workaround, we can still get it the original
+		// PortletRequest instance, because the Portlet spec says it must be stored in the javax.portlet.request
+		// attribute.
 		String queryString = null;
-		Object portletRequestAsObject = getRequestAttribute(REQUEST_ATTR_PORTLET_REQUEST);
+
+		// JSR-168/286 request attribute that contains an instance of javax.portlet.PortletRequest
+		Object portletRequestAsObject = getRequestAttribute("javax.portlet.request");
 
 		if ((portletRequestAsObject != null) && (portletRequestAsObject instanceof PortletRequest)) {
 			PortletRequest portletRequest = (PortletRequest) portletRequestAsObject;
@@ -75,30 +57,7 @@ public class FacesContextHelperPortletImpl extends FacesContextHelperImpl implem
 	}
 
 	@Override
-	public Object getSessionAttribute(String name) {
-		Object value = null;
-		PortletSession portletSession = (PortletSession) getSession(false);
-
-		if (portletSession != null) {
-			value = portletSession.getAttribute(name);
-		}
-
-		return value;
-	}
-
-	@Override
-	public void setRequestAttribute(String name, Object value) {
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-		PortletRequest portletRequest = (PortletRequest) externalContext.getRequest();
-		portletRequest.setAttribute(name, value);
-	}
-
-	@Override
-	public void setSessionAttribute(String name, Object value) {
-		PortletSession portletSession = (PortletSession) getSession(true);
-
-		if (portletSession != null) {
-			portletSession.setAttribute(name, value);
-		}
+	public FacesContextHelper getWrapped() {
+		return wrappedFacesContextHelper;
 	}
 }
