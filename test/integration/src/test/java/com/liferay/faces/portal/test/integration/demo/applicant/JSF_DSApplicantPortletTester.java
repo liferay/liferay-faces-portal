43d80576ede0514df7940e3192f7dfc4bcc5a819
Neil Griffin <neil.griffin.scm@gmail.com>
2017-04-21 15:01:36 -0400

    FACES-3057 Upgrade the liferay-faces-portal project to use the new test framework (code review)

--- test/integration/src/test/java/com/liferay/faces/portal/test/integration/demo/applicant/JSF_DSApplicantPortletTester.java
+++ test/integration/src/test/java/com/liferay/faces/portal/test/integration/demo/applicant/JSF_DSApplicantPortletTester.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.test.applicant;
+package com.liferay.faces.portal.test.integration.demo.applicant;
 
 import com.liferay.faces.test.selenium.TestUtil;
 import com.liferay.faces.test.selenium.applicant.ApplicantTesterBase;
