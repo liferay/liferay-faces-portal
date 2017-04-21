9935b9fff45329ce4bef9fe320c8647d6a9a69fe
pcwhite <philip.white@liferay.com>
2017-04-21 16:08:45 -0400

    FACES-3058 Rewrite FACES257PortletTest.java so that it uses the new test framework

--- test/integration/src/test/java/com/liferay/faces/portal/test/integration/issue/FACES_257PortletTester.java
+++ test/integration/src/test/java/com/liferay/faces/portal/test/integration/issue/FACES_257PortletTester.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 
 import com.liferay.faces.portal.test.integration.PortalTestUtil;
 import com.liferay.faces.test.selenium.Browser;
-import com.liferay.faces.test.selenium.TestUtil;
+import com.liferay.faces.test.selenium.assertion.SeleniumAssert;
 
 
 /**
@@ -25,4 +25,63 @@ import com.liferay.faces.test.selenium.TestUtil;
  */
 public class FACES_257PortletTester {
 
+	// alpha=1 beta=2 gamma=0
+	private static final String anchor1Xpath = "//a[contains(text(), 'alpha=1 beta=2 gamma=0')]";
+
+	private static final String assert1Xpath = anchor1Xpath +
+		"/following-sibling::*[1]/following-sibling::*[1]/following-sibling::*[1]/following-sibling::span[1]";
+
+	// alpha=1 beta=2 gamma=3
+	private static final String anchor2Xpath = "//a[contains(text(), 'alpha=1 beta=2 gamma=3')]";
+
+	private static final String assert2Xpath = anchor2Xpath +
+		"/following-sibling::*[1]/following-sibling::*[1]/following-sibling::*[1]/following-sibling::span[1]";
+
+	// alpha=4 beta=5 gamma=0
+	private static final String button1Xpath = "//input[@type='button' and contains(@value, 'alpha=4 beta=5 gamma=0')]";
+
+	private static final String assert3Xpath = button1Xpath +
+		"/following-sibling::*[1]/following-sibling::*[1]/following-sibling::span[1]";
+
+	// alpha=4 beta=5 gamma=6
+	private static final String button2Xpath = "//input[@type='button' and contains(@value, 'alpha=4 beta=5 gamma=6')]";
+
+	private static final String assert4Xpath = button2Xpath +
+		"/following-sibling::*[1]/following-sibling::*[1]/following-sibling::span[1]";
+
+	private static final String requestedUrlXpath = "//span[contains(@id, ':requestedURL')]";
+
+	@Test
+	public void runFACES_257PortletTest() {
+
+		Browser browser = Browser.getInstance();
+		browser.get(PortalTestUtil.getIssuePageURL("faces-257"));
+		browser.waitForElementVisible(anchor1Xpath);
+
+		// step 1
+		SeleniumAssert.assertElementVisible(browser, requestedUrlXpath);
+
+		browser.clickAndWaitForAjaxRerender(anchor1Xpath);
+
+		SeleniumAssert.assertElementVisible(browser, assert1Xpath);
+		browser.getCurrentUrl().contains(assert1Xpath);
+
+		// step 2
+		browser.clickAndWaitForAjaxRerender(anchor2Xpath);
+
+		SeleniumAssert.assertElementVisible(browser, assert2Xpath);
+		browser.getCurrentUrl().contains(assert2Xpath);
+
+		// step 3
+		browser.clickAndWaitForAjaxRerender(button1Xpath);
+
+		SeleniumAssert.assertElementVisible(browser, assert3Xpath);
+		browser.getCurrentUrl().contains(assert3Xpath);
+
+		// step 4
+		browser.clickAndWaitForAjaxRerender(button2Xpath);
+
+		SeleniumAssert.assertElementVisible(browser, assert4Xpath);
+		browser.getCurrentUrl().contains(assert4Xpath);
+	}
 }
