de1956c291135699f40e9266023bdf5045429506
Neil Griffin <neil.griffin.scm@gmail.com>
2017-04-21 15:01:36 -0400

    FACES-3057 Upgrade the liferay-faces-portal project to use the new test framework (code review)

--- test/integration/src/test/java/com/liferay/faces/portal/test/integration/PortalTestUtil.java
+++ test/integration/src/test/java/com/liferay/faces/portal/test/integration/PortalTestUtil.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 package com.liferay.faces.portal.test.integration;
 
@@ -25,13 +23,20 @@ import com.liferay.faces.test.selenium.TestUtil;
 public final class PortalTestUtil {
 
 	// Private Constants
-	private static final String DEFAULT_ISSUE_CONTEXT;
+	private static final String DEFAULT_DEMO_CONTEXT = "/group/portal-demos";
+	private static final String DEFAULT_ISSUE_CONTEXT = "/web/portal-issues";
 
-	static {
+	private PortalTestUtil() {
+		throw new AssertionError();
+	}
 
-		String defaultIssueContext = "/web/portal-issues";
+	public static String getDemoContext(String portletPageName) {
+		return TestUtil.getSystemPropertyOrDefault("integration.demo.context", DEFAULT_DEMO_CONTEXT) + "/" +
+			portletPageName;
+	}
 
-		DEFAULT_ISSUE_CONTEXT = defaultIssueContext;
+	public static String getDemoPageURL(String portletPageName) {
+		return TestUtil.DEFAULT_BASE_URL + getDemoContext(portletPageName);
 	}
 
 	public static String getIssueContext(String portletPageName) {
