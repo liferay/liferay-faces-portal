74273822ec8f0f9c6904934b65d2a7fd448a07a1
vsingleton <vsingleton@gmail.com>
2017-07-31 07:44:58 -0400

    FACES-3159 - Author integration test for portal:captcha

---
+++ demo/portal-showcase-portlet/src/main/java/com/liferay/faces/bridge/demos/validation/CaptchaTestValidatorBean.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.demos.validation;
+
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import com.liferay.faces.util.helper.BooleanHelper;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+@ManagedBean
+@RequestScoped
+public class CaptchaTestValidatorBean {
+
+	// Private Data Members
+	private String correctCaptchaValue;
+	private Boolean validateCaptchaForTest;
+
+	public String getCorrectCaptchaValue() {
+
+		if (correctCaptchaValue == null) {
+
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = facesContext.getExternalContext();
+			Map<String, Object> sessionMap = externalContext.getSessionMap();
+			correctCaptchaValue = (String) sessionMap.get("CAPTCHA_TEXT");
+
+			if (correctCaptchaValue == null) {
+				return "Correct Captcha Value";
+			}
+		}
+
+		return correctCaptchaValue;
+	}
+
+	public boolean isValidateCaptchaForTest() {
+
+		if (validateCaptchaForTest == null) {
+
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = facesContext.getExternalContext();
+			String initParameterValue = externalContext.getInitParameter("VALIDATE_CAPTCHA_FOR_TEST");
+
+			validateCaptchaForTest = Boolean.valueOf(BooleanHelper.isTrueToken(initParameterValue));
+		}
+
+		return validateCaptchaForTest;
+	}
+}
