a3df4dd8b9b264ca3d0b884bd83e10d1fb632f32
Neil Griffin <neil.griffin.scm@gmail.com>
2017-08-04 16:21:54 -0400

    FACES-3159 - Author integration test for portal:captcha (refresh button test - code review)

--- test/integration/src/test/java/com/liferay/faces/portal/test/showcase/captcha/CaptchaGeneralTesterCompat.java
+++ test/integration/src/test/java/com/liferay/faces/portal/test/showcase/captcha/CaptchaGeneralTesterCompat.java
@@ -13,6 +13,7 @@
  */
 package com.liferay.faces.portal.test.showcase.captcha;
 
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 
@@ -50,7 +51,7 @@ public abstract class CaptchaGeneralTesterCompat extends InputTextTester {
 			navigateToUseCase(browserDriver, "captcha", "general");
 			browserDriver.clickElement(requiredCheckbox1Xpath);
 
-			// 2. Enter the correct value into the *Captcha* field.
+			// 2. Enter the value that is displayed in the distorted captcha image into the *Captcha* text field.
 			String correctCaptchaValue = getCorrectCaptchaValue(browserDriver);
 			browserDriver.clearElement(CAPTCHA_INPUT_XPATH);
 			browserDriver.sendKeysToElement(CAPTCHA_INPUT_XPATH, correctCaptchaValue);
@@ -75,6 +76,62 @@ public abstract class CaptchaGeneralTesterCompat extends InputTextTester {
 			PortalTestUtil.signOut(browserDriver);
 		}
 
+	}
+
+	/*
+	 * This test is to make sure that the fix for LPS-65011 does not regress. It also tests the refresh button for the
+	 * SimpleCaptcha.
+	 */
+	@Test
+	public void runCaptchaGeneralTest_D_NonAuthenticated_IncorrectCaptchaAfterRefreshAndSubmit() throws Exception {
+
+		BrowserDriver browserDriver = getBrowserDriver();
+		WaitingAsserter waitingAsserter = getWaitingAsserter();
+		navigateToUseCase(browserDriver, "captcha", "general");
+
+		// 1. Take note of the first value that is displayed in the distorted captcha image.
+		String correctCaptchaValue1 = getCorrectCaptchaValue(browserDriver);
+
+		// 2. Enter the first value into the *Captcha* text field.
+		browserDriver.clearElement(CAPTCHA_INPUT_XPATH);
+		browserDriver.sendKeysToElement(CAPTCHA_INPUT_XPATH, correctCaptchaValue1);
+
+		// 3. Click on the *Submit* button and verify that an informational message is displayed indicating that the
+		// correct value was submitted.
+		browserDriver.clickElementAndWaitForRerender(submitButton1Xpath);
+		waitingAsserter.assertElementDisplayed(CAPTCHA_MSG_INFO_XPATH);
+		waitingAsserter.assertTextPresentInElement("You entered the correct text verification code",
+			CAPTCHA_MSG_INFO_XPATH);
+
+		// 4. Click on the *Refresh CAPTCHA* icon.
+		String refreshCaptchaXpath = "//a[contains(@id,'refreshCaptcha')]";
+		waitingAsserter.assertElementDisplayed(refreshCaptchaXpath);
+		browserDriver.clickElement(refreshCaptchaXpath);
+		browserDriver.waitForElementEnabled("//div[contains(@class,'taglib-captcha')]/img[contains(@id,'captcha')]",
+			true);
+
+		// 5. Take note of the second value that is displayed in the distorted captcha image.
+		String correctCaptchaValue2 = getCorrectCaptchaValue(browserDriver);
+
+		// 6. Enter the second value into the *Captcha* text field.
+		browserDriver.clearElement(CAPTCHA_INPUT_XPATH);
+		browserDriver.sendKeysToElement(CAPTCHA_INPUT_XPATH, correctCaptchaValue2);
+
+		// 7. Click on the *Submit* button and verify that an informational message is displayed indicating that the
+		// correct value was submitted.
+		browserDriver.clickElementAndWaitForRerender(submitButton1Xpath);
+		waitingAsserter.assertElementDisplayed(CAPTCHA_MSG_INFO_XPATH);
+		waitingAsserter.assertTextPresentInElement("You entered the correct text verification code",
+			CAPTCHA_MSG_INFO_XPATH);
+
+		// 8. Take note of the third value that is displayed in the distorted captcha image.
+		String correctCaptchaValue3 = getCorrectCaptchaValue(browserDriver);
+
+		// 9. Verify that the third value is not the same as the first one.
+		Assert.assertNotEquals(correctCaptchaValue1, correctCaptchaValue3);
+
+		// 10. Verify that the third value is not the same as the second one.
+		Assert.assertNotEquals(correctCaptchaValue2, correctCaptchaValue3);
 	}
 
 	protected abstract String getCorrectCaptchaValue(BrowserDriver browserDriver);
