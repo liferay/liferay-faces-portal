f28df982725f4c58b84ce7dd4a4927c46d1a3c82
Neil Griffin <neil.griffin.scm@gmail.com>
2017-08-21 17:17:58 -0400

    FACES-3166 - portal:captcha does not support reCAPTCHA (code review -- compatibility layer for backport)

--- portal/src/main/java/com/liferay/faces/portal/component/captcha/internal/CaptchaRenderer.java
+++ portal/src/main/java/com/liferay/faces/portal/component/captcha/internal/CaptchaRenderer.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 package com.liferay.faces.portal.component.captcha.internal;
 
@@ -173,7 +171,7 @@ public class CaptchaRenderer extends CaptchaRendererCompat {
 		String namespace = portletResponse.getNamespace();
 		String textToReplace = "id=\"refreshCaptcha\"";
 		String replacement = "id=\"".concat(namespace).concat("refreshCaptcha\"");
-		String modifiedMarkup = markup.toString();
+		String modifiedMarkup = fixMarkup(markup.toString());
 		modifiedMarkup = modifiedMarkup.replace(textToReplace, replacement);
 
 		// Note: It is only possible to prepend the hidden field with the portlet namespace for simple captcha since the
@@ -182,9 +180,8 @@ public class CaptchaRenderer extends CaptchaRendererCompat {
 		if (getCaptchaType() == CaptchaType.SIMPLE) {
 			replacement = "name=\"".concat(namespace).concat("captchaText\"");
 			textToReplace = "name=\"captchaText\"";
+			modifiedMarkup = modifiedMarkup.replace(textToReplace, replacement);
 		}
-
-		modifiedMarkup = modifiedMarkup.replace(textToReplace, replacement);
 
 		// Remove <label>Text Verification<span class="required">...</span></label> since it is not possible to
 		// customize the "Text Verification" label in the JSP tag. Better to let JSF developers decorate portal:captcha
--- portal/src/main/java/com/liferay/faces/portal/component/captcha/internal/CaptchaRendererCompat.java
+++ portal/src/main/java/com/liferay/faces/portal/component/captcha/internal/CaptchaRendererCompat.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 package com.liferay.faces.portal.component.captcha.internal;
 
@@ -34,6 +32,37 @@ public abstract class CaptchaRendererCompat extends DelayedPortalTagRenderer<Cap
 
 	protected enum CaptchaType {
 		SIMPLE, RECAPTCHA
+	}
+
+	protected String fixMarkup(String markup) throws Exception {
+
+		// Note: The following reCAPTCHA script has surrounding CDATA markers that must be removed in order for jsf.js
+		// to be able to parse the partial-response as a well-formed XML document.
+
+		//J-
+		// // <![CDATA["
+		// <script type="text/javascript">
+		// var RecaptchaOptions = { lang : 'en', theme : 'white' };
+		// </script>
+		// // ]]>
+		//J+
+
+		if (getCaptchaType() == CaptchaType.RECAPTCHA) {
+			String token = "// <![CDATA[";
+			int pos = markup.indexOf(token);
+
+			if (pos > 0) {
+				markup = markup.substring(0, pos) + markup.substring(pos + token.length());
+				token = "// ]]>";
+				pos = markup.indexOf(token);
+
+				if (pos > 0) {
+					markup = markup.substring(0, pos) + markup.substring(pos + token.length());
+				}
+			}
+		}
+
+		return markup;
 	}
 
 	protected CaptchaType getCaptchaType() {
