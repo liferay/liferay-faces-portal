347f6c236f15f3442bd5ef945696da484d2db96b
Neil Griffin <neil.griffin.scm@gmail.com>
2017-08-21 16:15:44 -0400

    FACES-3166 - portal:captcha does not support reCAPTCHA (code review -- compatibility layer for backport)

---
+++ portal/src/main/java/com/liferay/faces/portal/component/captcha/internal/CaptchaRendererCompat.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portal.component.captcha.internal;
+
+import com.liferay.captcha.taglib.servlet.taglib.CaptchaTag;
+
+import com.liferay.faces.portal.component.captcha.Captcha;
+import com.liferay.faces.portal.render.internal.DelayedPortalTagRenderer;
+
+import com.liferay.portal.kernel.captcha.CaptchaUtil;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class CaptchaRendererCompat extends DelayedPortalTagRenderer<Captcha, CaptchaTag> {
+
+	protected enum CaptchaType {
+		SIMPLE, RECAPTCHA
+	}
+
+	protected CaptchaType getCaptchaType() {
+
+		String captchaImpl = CaptchaUtil.getCaptcha().getClass().getName();
+
+		if ((captchaImpl != null) && captchaImpl.contains("ReCaptcha")) {
+			return CaptchaType.RECAPTCHA;
+		}
+		else {
+			return CaptchaType.SIMPLE;
+		}
+	}
+}
