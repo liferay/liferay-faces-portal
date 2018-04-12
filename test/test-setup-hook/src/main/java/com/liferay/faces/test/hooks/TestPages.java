8a8146566b0184389380fcc680185d1aec45ca91
Kyle Stiemann <stiemannkj1@gmail.com>
2018-04-12 09:08:57 -0400

    FACES-3259 Remove support for PrimePush

--- test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestPages.java
+++ test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestPages.java
@@ -81,8 +81,6 @@ public class TestPages {
 				new Portlet("1", "com.liferay.faces.demo.alloy.applicant.portlet")));
 		BRIDGE_DEMO_PAGES.add(new PortalPage("primefaces-applicant",
 				new Portlet("1", "com.liferay.faces.demo.primefaces.applicant.portlet")));
-		BRIDGE_DEMO_PAGES.add(new PortalPage("primepush-cdi-counter",
-				new Portlet("1", "com.liferay.faces.demo.primepush.cdi.counter.portlet")));
 		BRIDGE_DEMO_PAGES.add(new PortalPage("richfaces-applicant",
 			new Portlet("1", "com.liferay.faces.demo.richfaces.applicant.portlet")));
 		BRIDGE_DEMO_PAGES.add(new PortalPage("bootsfaces-applicant",
