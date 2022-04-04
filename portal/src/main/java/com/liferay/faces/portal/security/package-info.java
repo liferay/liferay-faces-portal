2f956ee194468ad477215041f8311b9c5e266b61
Neil Griffin <neil.griffin.scm@gmail.com>
2022-04-04 17:47:30 -0400

    Copyright year

--- portal/src/main/java/com/liferay/faces/portal/security/package-info.java
+++ portal/src/main/java/com/liferay/faces/portal/security/package-info.java
@@ -11,5 +11,5 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-@org.osgi.annotation.versioning.Version("4.1.0")
+@org.osgi.annotation.versioning.Version("4.1.1")
 package com.liferay.faces.portal.security;
